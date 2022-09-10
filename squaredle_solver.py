68ab9a8ac9f5f563fa3f12c4f644762a11d21388
Marcus Schweizer <marcus.schweizer@bioeye.com>
2022-09-10 19:27:25 +1000

    Word Class, dict doesn't cover scenarios

--- squaredle_solver.py
+++ squaredle_solver.py
@@ -20,26 +20,35 @@ class Word:
         return '%s - %s' % (self.word, self.positions)
 
 
-def get_adj(board, word):
+def get_adj(board, input_word):
     
     #adj_words = []
 
     # row and column of last pos 
-    r = word.positions[-1][0]
-    c = word.positions[-1][1]
+    #word_item = next(iter(input_word))
+    
+    word = input_word.word
+    positions = input_word.positions
+
+    r = positions[-1][0]
+    c = positions[-1][1]
+    print("a ", word, " - ", positions)
 
     # create the 3x3 matrix with all adjacent position tuples
     adj_pos = [[(r+y,c+x) for x in range(-1,2)] for y in range(-1,2)]
-
+    print("b ",adj_pos)
     # flatten the matrix to a list and
-    # filter for exists on board (deals with nulls or board edges) and is not an alread used positions
-    available = lambda x : x in board and x not in word.positions
-    adj_pos = [val for sublist in adj_pos for val in filter(available, sublist) ] 
-    #adj_pos = [val for sublist in adj_pos for val in sublist if val in board and val not in word.positions ] 
 
+    # filter for exists on board (deals with nulls or board edges) and is not an alread used positions
+    available = lambda x : x in board and x not in positions
+    adj_pos = [val for sublist in adj_pos for val in filter(available, sublist) ] 
+    #adj_pos = [val for sublist in adj_pos for val in sublist if val in board and val not in word.positions ]
+    print("c ",adj_pos)
+    
     #create a list of adjacent words based with new letter appended and new position appended to position list
-    adj_words = [(Word(word.word + board[newPos], word.positions + [newPos])) for newPos in adj_pos ]        
-            
+    adj_words = [(Word(word + board[newPos], positions + [newPos])) for newPos in adj_pos ]        
+
+    print("e ", word, " - ", adj_words)
     return adj_words
 
 
@@ -50,7 +59,11 @@ def find_words(board, word, min_length):
     adj_words = []
     if word == []:
         #get starting positions
-        adj_words = [Word(board_dict[pos], [pos]) for pos in board_dict]
+        for pos in board_dict:
+            # need to have the unique starting position with otherwise similar letters are overwritten
+            new_word = Word(board_dict[pos], [pos]) 
+            adj_words.append(new_word)
+        print("Starting words", len(adj_words))
         
     else:
         #get a list of all words from all available adjacent letters
@@ -60,15 +73,22 @@ def find_words(board, word, min_length):
 
     #print(len(adj_words))
     return_words = []
+    print("other ", adj_words)
+    for word_item in adj_words:
 
-    for adj_word in adj_words:
-        the_word = adj_word.word
+        the_word = word_item.word
+        the_positions = word_item.positions
+
+        if len(the_word) >= min_length and wordnet.synsets(the_word) and the_word not in [w.word for w in return_words]:
+            print("FOUND c ", the_word, the_positions)
+            return_words.append(word_item)
         
-        if len(the_word) >= min_length and wordnet.synsets(the_word) and adj_word not in return_words:
-            print("FOUND c ", adj_word)
-            return_words.append(adj_word)
 
-        return_words.extend(find_words(board, adj_word, min_length))
+        next_words = find_words(board, word_item , min_length)
+
+        #append next words whilst checking that it doesn't exist in words    
+        return_words = return_words + [n for n in next_words if n.word not in [w.word for w in return_words]]
+    
         
     return return_words
 
